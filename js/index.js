6877ec7f5c60d35008f2d9a101ffa14fabb035ed
murtuzzov <murtuzzov>
2024-03-01 23:31:29 +0400

    first commit

---
+++ js/index.js
@@ -0,0 +1,71 @@
+// ЗАДАЧА 1 | найти сумму положительных
+// элементов в массиве
+// const numbersArr = [1,-2,3,-4,5];
+// let sum = 0;
+// for(let i = 0; i<numbersArr.length; i+=1){
+//  if(numbersArr[i] < 0){
+//    continue;
+//  }else {
+//    sum+=numbersArr[i];
+//  }
+// }
+// console.log(sum)
+///////////////////////////////////////////////////////////////////////////////////
+// ЗАДАЧА No2 | найти количество
+// отрицательных элементов в массиве
+// const numbersArr = [1,2,-3,-4,5,6,-7,8]
+// let negativeNum = 0;
+
+// for ( let i = 0; i<numbersArr.length; i+=1){
+//  if(numbersArr[i] < 0){
+//    negativeNum+=1
+//  }
+// }
+// console.log(negativeNum)
+////////////////////////////////////////////////////////////////////////////////////
+//ЗАДАЧА No3 | отфильтровать массив,
+//оставив только уникальные значения
+//const numbersArr = [1,2,2,4]
+//const newNumArr = []
+//for(let i = 0; i<numbersArr.length; i+=1){
+//  if(!newNumArr.includes(numbersArr[i])){
+//    newNumArr.push(numbersArr[i])
+//  }
+//}
+//console.log(newNumArr)
+////////////////////////////////////////////////////////////////////////////////////
+//ЗАДАЧА No4 | найти сумму элементов
+//на четных позициях в массиве
+//индекс начинается с нуля
+//const numbersArr = [1,2,3,4,5,6,7,8];
+//let sum=0;
+//for(let i = 0; i<numbersArr.length; i+=1){
+//  if(numbersArr[i] % 2 !==0 ){
+//    continue;
+//  } else {
+//    sum+=numbersArr[i]
+//  }
+//}
+//console.log(sum)
+/////////////////////////////////////////////////////////////////////////////////
+//ЗАДАЧА No5 | проверить, является ли каждый
+//элемент массива целым числом
+//const numbersArr = [4, -9, 2.5, 12, -1];
+//for (let i = 0; i < numbersArr.length; i += 1) {
+//  if (numbersArr[i] % 1 === 0) {
+//    console.log(numbersArr[i] + " является целым числом.");
+//  } else {
+//    console.log(numbersArr[i] + " не является целым числом.");
+//  }
+//}
+///////////////////////////////////////////////////////////////////////////////
+//ЗАДАЧА No6 | найти индекс первого
+//отрицательного элемента в массиве
+//const numbersArr = [3,9,5,-7,6];
+//let index = 0;
+//for(let i = 0; i<numbersArr.length; i+=1){
+//  if(numbersArr[i]<0){
+//   index = i
+//  }
+//}
+//console.log(Отрицательный элемент массива находится на ${index} индексе)
